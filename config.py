908f2eb5c670fd0c507c0db45f1ce553f33f9a59
Ahimsa Das <das.ahimsa@gmail.com>
2023-05-02 18:40:15 +0530

    Added tweet functionality 🐦

--- config.py
+++ config.py
@@ -8,8 +8,15 @@ query_topic = "crypto"
 # The complete value for q must be URL-encoded. Max length: 500 chars.
 
 # SECRETS
-openai_api_key          = "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"
-openai_organization_key = "org-FyPhgskwwxBLTTt9nIPjy4gD"
-newsapi_api_key         = "cc5426462a664c0399c218ae0bdfd1b9"
+openai_api_key              = "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"
+openai_organization_key     = "org-FyPhgskwwxBLTTt9nIPjy4gD"
+newsapi_api_key             = "cc5426462a664c0399c218ae0bdfd1b9"
+TWITTER_API_KEY             = 'S9DlzxrQVnhkC7umCq4HCcZgS'
+TWITTER_API_SECRET_KEY      = "TV9BrSpAEXB3GcNC5VxFTuS5wQuT4rVdyN50R3tLDpzL9QRxEC"
+# TWITTER_CLIENT_ID           = "Umt2NjhGWnZ4TnF2UFhkZEZ3Q2o6MTpjaQ"
+# TWITTER_CLIENT_SECRET       = "kJxtp-0cCZk8ZDjFGEOmzRo5-Qbgr4DrpQ9YdKsv6QaPNOWCqF"
+TWITTER_ACCESS_TOKEN        = "3180326750-Ka9zltqvg6bZAwdWrPyjIE9qbloyGqd8hTsAHrL"
+TWITTER_ACCESS_TOKEN_SECRET = "UI1kfi7xWnVJmcFCQOwJddZi8pKz5NYGIPfbgcQbxCENt"
+# TWITTER_BEARER_TOKEN        = "AAAAAAAAAAAAAAAAAAAAANa2nAEAAAAAlGEO4ySucJ3pZjn3e%2BzFzIYrcnc%3DHmkvHIXw1hg8jQJELntbzKUrJCjMIQfM59tMm9cQJS7B8ECXLa"
 
-chatGPT_prompt = "Compose tweets based on the news articles provided below in a sarcastic, satirical, and funny tone. Return the output in the given JSON format.{\"generated_tweets\": [{\"content\": \"<tweet>\"},{\"content\": \"<tweet>\"},...]}Append relevant hashtags and the respective URL of the article at the end of each <tweet>. Do not shorten the URLs and also throw in some emojis in the <tweet>"
+chatGPT_prompt = "Compose tweet(s) based on the news article(s) provided below in a sarcastic, satirical, and funny tone. Return the output in the given JSON format {\"generated_tweets\": [{\"content\": \"<tweet>\"},{\"content\": \"<tweet>\"},...]} Append relevant hashtags and the respective URL of the article at the end of each <tweet>. Each <tweet> should be in this format '<tweet_text> <relevant_hashtags> <URL>'. Do not shorten the URLs. Throw in some emojis in the <tweet_text>. If only one URL is provided return only one <tweet>."
--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import requests
 import json
 import config
+import tweepy
 import openai
 openai.organization = config.openai_organization_key
 openai.api_key = config.openai_api_key
 openai.Model.list()
 
 url = f"https://newsapi.org/v2/everything?pageSize={config.number_of_tweets}&q={config.query_topic}&apiKey={config.newsapi_api_key}"
+
+client = tweepy.Client(
+    consumer_key        = config.TWITTER_API_KEY,
+    consumer_secret     = config.TWITTER_API_SECRET_KEY,
+    access_token        = config.TWITTER_ACCESS_TOKEN,
+    access_token_secret = config.TWITTER_ACCESS_TOKEN_SECRET
+)
 
 # Fetch the JSON data from the URL
 response = requests.get(url)
@@ -21,6 +29,8 @@ if response.status_code == 200:
     urls = []
     for article in data['articles']:
         urls.append(article['url'])
+
+    print("DEBUG : Total Number of articles received = ",len(urls),"\n")
 
     # Append URLs in chatGPT prompt
     for url in urls:
@@ -43,11 +53,17 @@ if response.status_code == 200:
         message = completions.choices[0].text
         return message.strip()
 
+    print("DEBUG : chatGPT_prompt = ",chatGPT_prompt,"\n")
+
     # Processing the response
     chatGPT_response = ask_gpt(prompt)
 
+    print("DEBUG : chatGPT_response = ",chatGPT_response,"\n")
+
     # Converting response string to JSON
     chatGPT_json_response = json.loads(chatGPT_response)
+
+    print("DEBUG : chatGPT_json_response = ",chatGPT_json_response,"\n")
 
     # Extracting the tweets array
     tweets = []
@@ -57,5 +73,6 @@ if response.status_code == 200:
     # Processing each tweet
     for item in tweets:
         print(item,"\n")
+        client.create_tweet(text=item)
 else:
     print("Error fetching data: ", response.status_code)
